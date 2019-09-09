c96d654cfe5c8f071ebb6edb190d368d054090e2
Merith <merith.tk@gmail.com>
2019-09-09 11:54:43 -0700

    Rewrite Code.go

--- Code.go
+++ Code.go
@@ -1,45 +1,23 @@
 package main
 
 import (
-	"fmt"
-	"log"
 	"os"
 	"os/exec"
-	"path/filepath"
-	"strings"
 )
 
-// Set universal variables
-var workspace = "/Documents/Workspace"
-var err error
-var path = "/PortableApps/CommonFiles/bin"
+var _workspace = "/Documents/Workspace"
+var _path = "/PortableApps/msys64/mingw64/bin"
 
 func main() {
+	// Setup Path
+	os.Setenv("PATH", _path)
 
-	// If there is a specifed folder, use that was workspace
+	// If a file or folder is provided, open that
 	if len(os.Args) >= 2 {
-		workspace = os.Args[1]
+		_workspace = os.Args[1]
 	}
 
-	// Get direct path for windows to `workspace`
-	workspace, err = filepath.Abs(workspace)
-	if err != nil {
-		log.Fatal(err)
-	}
-	// Get direct path for windows to `path`
-	path, err = filepath.Abs(path)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Clean workspace path
-	workspace = strings.Replace(workspace, "\\PortableApps\\msys64", "", -1)
-
-	//Set PATH
-	os.Setenv("PATH", path)
-	
-	//Execute Code with workspace
-	fmt.Println("Starting Code Portable:", workspace)
-	cmd := exec.Command("/PortableApps/Code/Code.exe", workspace)
+	// Launch VSCode
+	cmd := exec.Command("/PortableApps/Code/Code.exe", _workspace)
 	cmd.Start()
 }
